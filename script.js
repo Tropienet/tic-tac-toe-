c68c4cd51be732b2d02d60e8c159118a019b679a
Tin Goricki <exibodude@gmail.com>
2023-03-11 17:11:36 +0100

    Made a module to display the board in console

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const playerFactory = (name, mark) => {
 const tin = playerFactory("tin", "x");
 
 tin.sayHello();
+
+const gameBoard = (() => {
+    const board = [[1,2,3], 
+                   [4,5,6],
+                   [7,8,9]];
+    return  {
+        board
+    };
+})();
+
+const game = (() => {
+    const start = (board) => board.forEach(row => {
+        row.forEach(space => {
+            console.log(space);
+        })
+    });
+    return {
+        start,
+    };
+})();
+
+game.start(gameBoard.board);
